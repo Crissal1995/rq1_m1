6101a4ba659b1d957a705ce2766a20c81e2bcec0
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-24 17:52:42 +0100

    Added constants + outputs

--- src/major/main.py
+++ src/major/main.py
@@ -4,6 +4,22 @@ import logging
 from src.major.generate_live_mutants import get_live_mutants, get_directory
 from src.major.get_coupled_mutants import get_mutants_from_set, create_set_mutants
 
+
+"""
+major scheme for mutation (mutants.log)
+
+mutant_id, mutant_operator, original, mutation, function, line, full_mutation
+    0              1             2        3          4      5         6
+"""
+ID = 0
+OPERATOR = 1
+ORIGINAL = 2
+MUTATION = 3
+FUNCTION = 4
+LINE = 5
+FULL_MUTATION = 6
+
+
 FORMAT_VERBOSE = "%(levelname)s::%(module)s::%(lineno)d::%(asctime)s::%(message)s"
 FORMAT = "%(levelname)s...%(asctime)s...%(message)s"
 
@@ -25,14 +41,20 @@ fname = "generated_live_mutants.log"
 buggy_mutants = get_live_mutants(buggy_dir)
 fixed_mutants = get_live_mutants(fixed_dir)
 
-set_keys = (1, 2, 4)
+set_keys = (
+    LINE,
+    OPERATOR,
+    ORIGINAL,
+    MUTATION,
+    FULL_MUTATION
+)
 buggy_set = create_set_mutants(buggy_mutants, set_keys)
 fixed_set = create_set_mutants(fixed_mutants, set_keys)
 diff_set = buggy_set - fixed_set
 
-diff_mutants = get_mutants_from_set(buggy_mutants, diff_set, set_keys, sort_on=0)
+diff_mutants = get_mutants_from_set(buggy_mutants, diff_set, set_keys, sort_on=ID)
 
-print(f"Caught {len(diff_set)} mutants in the diff set")
+logging.info(f"Caught {len(diff_set)} mutants in the diff set")
 
 with open(root_dir / "diffset.log", "w") as f:
     f.writelines("...".join(mutant) + "\n" for mutant in diff_mutants)
