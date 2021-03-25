0fe96375da98b9e98a5d4843540b4cd612e87c47
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-25 16:00:56 +0100

    Add classnames to utility

--- src/utility.py
+++ src/utility.py
@@ -3,7 +3,12 @@ import pathlib
 # file.py -> src dir -> root dir
 HOME = pathlib.Path(__file__).parent.parent
 
-subjects = {'cli': 'cli32', 'lang': 'lang53', 'gson': 'gson15'}
+subjects = {"cli": "cli32", "lang": "lang53", "gson": "gson15"}
+classnames = {
+    "cli": "org.apache.commons.cli.HelpFormatter",
+    "gson": "com.google.gson.stream.JsonWriter",
+    "lang": "org.apache.commons.lang.time.DateUtils"
+}
 tools = ["judy", "jumble", "major", "pit"]
 
 
@@ -11,3 +16,8 @@ def get_root_dir(subject: str, tool: str):
     assert subject in subjects
     assert tool in tools
     return HOME / "data" / subjects[subject] / tool
+
+
+def get_class_name(subject: str):
+    assert subject in classnames
+    return classnames[subject]
