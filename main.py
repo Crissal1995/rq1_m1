0e0327d36591c52aeaf014cb04825406e7faeeb4
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-03-29 16:48:59 +0200

    Added logging + cycle over tools AND subjects

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import logging
+
 from src import utility
 
-for tool in ("judy", "jumble", "major", "pit"):
-    factory = utility.ReportFactory(tool=tool, subject="cli")
+FORMAT = "%(levelname)s :: %(asctime)s :: %(module)s, line %(lineno)d :: %(message)s"
 
-    if not factory.is_valid():
-        continue
+logging.basicConfig(filename="main.log", format=FORMAT, level=logging.DEBUG)
 
-    factory.write_mutants()
+file_handler = logging.FileHandler("main.log")
+file_handler.setFormatter(logging.Formatter(FORMAT))
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(logging.Formatter(FORMAT))
+
+logging.getLogger().addHandler(file_handler)
+logging.getLogger().addHandler(stream_handler)
+
+for tool in utility.tools:
+    for subject in utility.subjects:
+        try:
+            factory = utility.ReportFactory(tool=tool, subject=subject)
+        except Exception as e:
+            logging.error(f"TOOL: {tool}, SUBJECT: {subject}, ERROR: {e}")
+        else:
+            factory.write_mutants()
