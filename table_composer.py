47beca98e3473fa33ac6c3d172825316e859b78d
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-06-30 16:18:44 +0200

    Created another py file to get effectiveness
    At the moment judy is removed - we don't have any data

---
+++ table_composer.py
@@ -0,0 +1,50 @@
+import logging
+
+import pandas as pd
+
+from src.utility import subjects, tools
+
+
+def main(root_dir: str = "data_cmp"):
+    valid_tools = [tool for tool in tools if tool != "judy"]
+
+    dataframes = []
+
+    for subject in subjects:
+        for tool in valid_tools:
+            csvpath = f"{root_dir}/{subject}/{tool}/{subject}_{tool}.csv"
+            df = pd.read_csv(csvpath, index_col=0)
+
+            ddf = pd.DataFrame(df.count(axis=1), columns=["live_count"])
+            ddf["Group"] = ddf.index.str.extract(r"(\w\d).*").set_index(ddf.index)
+            ddf["Tool"] = tool.capitalize()
+            ddf["Project"] = subject.capitalize()
+
+            # calculate effectiveness as 1 - live achieved / live total
+            ddf["effectiveness"] = 1 - ddf["live_count"] / ddf["live_count"].max()
+
+            x = (
+                ddf.dropna()
+                .reset_index(drop=True)
+                .set_index(["Tool", "Group", "Project"])
+            )
+            dataframes.append(x)
+
+    y = pd.concat(dataframes, axis=1)
+    yy = y.apply(
+        lambda row: [el for el in row if el and not pd.isna(el)][:2],
+        axis=1,
+        result_type="expand",
+    )
+    yy.columns = ["live_count", "effectiveness"]
+    yy.to_csv(f"{root_dir}/effectiveness.csv")
+    print(yy)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(levelname)s :: [%(module)s.%(lineno)d] :: %(message)s",
+        level=logging.INFO,
+    )
+
+    main()
