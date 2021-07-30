b8c5abe3f7da7fc2448fb27955d451bd60e248b8
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-07-30 11:29:53 +0200

    Changed main to run all over the projects

--- newmutants_main.py
+++ newmutants_main.py
@@ -2,21 +2,28 @@ import pathlib
 
 from src.mutants import reports
 
-p = pathlib.Path("data_cmp/cli/judy/_single_dev/result.json")
-report = reports.JudyReport.from_file(
-    p, class_under_mutation="org.apache.commons.cli.HelpFormatter"
-)
-print(report)
+projects = ["cli", "gson", "lang"]
+classes = [
+    "org.apache.commons.cli.HelpFormatter",
+    "com.google.gson.stream.JsonWriter",
+    "org.apache.commons.lang.time.DateUtils",
+]
 
-p = pathlib.Path("data_cmp/cli/jumble/_single_dev/jumble_output.txt")
-report = reports.JumbleReport.from_file(p)
-print(report)
 
-p1 = pathlib.Path("data_cmp/cli/major/_single_dev/kill.csv")
-p2 = pathlib.Path("data_cmp/cli/major/_single_dev/mutants.log")
-report = reports.MajorReport.from_files([p1, p2])
-print(report)
+for project, cls in zip(projects, classes):
+    p = pathlib.Path(f"data_cmp/{project}/judy/_single_dev/result.json")
+    report = reports.JudyReport.from_file(p, class_under_mutation=cls)
+    print(report)
 
-p = pathlib.Path("data_cmp/cli/pit/_single_dev/mutations.xml")
-report = reports.PitReport.from_file(p)
-print(report)
+    p = pathlib.Path(f"data_cmp/{project}/jumble/_single_dev/jumble_output.txt")
+    report = reports.JumbleReport.from_file(p)
+    print(report)
+
+    p1 = pathlib.Path(f"data_cmp/{project}/major/_single_dev/kill.csv")
+    p2 = pathlib.Path(f"data_cmp/{project}/major/_single_dev/mutants.log")
+    report = reports.MajorReport.from_files([p1, p2])
+    print(report)
+
+    p = pathlib.Path(f"data_cmp/{project}/pit/_single_dev/mutations.xml")
+    report = reports.PitReport.from_file(p)
+    print(report)
