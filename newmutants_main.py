18a36c8f7d05a41ae3b089f0c28ea3288cce8137
Cristiano Salerno <cristianosalerno1@gmail.com>
2021-07-31 12:42:49 +0200

    changed old main imports

--- newmutants_main.py
+++ newmutants_main.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from src.mutants import reports
+from reports.reports import JudyReport, JumbleReport, MajorReport, PitReport
 
 projects = ["cli", "gson", "lang"]
 classes = [
@@ -14,20 +14,20 @@ for project, cls in zip(projects, classes):
     print(f"WORKING WITH {project}")
 
     p = pathlib.Path(f"data_cmp/{project}/judy/_single_dev/result.json")
-    report = reports.JudyReport(p, cls)
+    report = JudyReport(p, cls)
     print(report.summary(), "\n")
 
     p = pathlib.Path(f"data_cmp/{project}/jumble/_single_dev/jumble_output.txt")
-    report = reports.JumbleReport(p)
+    report = JumbleReport(p)
     print(report.summary(), "\n")
 
     p1 = pathlib.Path(f"data_cmp/{project}/major/_single_dev/kill.csv")
     p2 = pathlib.Path(f"data_cmp/{project}/major/_single_dev/mutants.log")
-    report = reports.MajorReport(p2, p1)
+    report = MajorReport(p2, p1)
     print(report.summary(), "\n")
 
     p = pathlib.Path(f"data_cmp/{project}/pit/_single_dev/mutations.xml")
-    report = reports.PitReport(p)
+    report = PitReport(p)
     print(report.summary(), "\n")
 
     print("-" * 60)
